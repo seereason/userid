cfa4f9af6f752aec94e70817614cffdecafca551
Clifford Beshers <beshers@seereason.com>
2026-01-14 10:24:21 -0800

    add a flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      overlay = final: prev: {
+        userid = final.callCabal2nix "userid" ./. { };
+        userid = userid;
+      };
+      myHaskellPackages = pkgs.haskellPackages.extend overlay;
+    in
+    {
+      packages.${system}.default = myHaskellPackages.userid;
+    };
+}
